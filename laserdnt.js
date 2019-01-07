10cf7370753e3934a93e14cd73a499ddfc4a66b8
Roshan George <roshan@arjie.com>
2019-01-06 23:32:04 -0800

    Load the blacklist when we start

--- laserdnt.js
+++ laserdnt.js
@@ -1,1 +1,12 @@
 var blacklistedHosts = new Set([]);
+
+function prepBlacklist(hostString) {
+  return new Set(hostString.split(',').map(x => x.trim()));
+}
+
+chrome.storage.sync.get({
+  laserDntHosts: ''
+}, function(items) {
+  blacklistedHosts = prepBlacklist(items.laserDntHosts);
+  console.log("Loaded blacklist: " + Array.from(blacklistedHosts.values()));
+});
